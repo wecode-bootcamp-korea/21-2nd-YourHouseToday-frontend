8b4087e5f3872e349c41316a5bae6f6657480baa
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-06-21 18:44:05 +0900

    Modify : 초기세팅 2차  수정 완료

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -4,6 +4,27 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset};
 
+* {
+  box-sizing: border-box;
+}
+button {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+img {
+  object-fit: cover;
+}
+input[type='checkbox'] {
+  cursor: pointer;
+}
+a {
+  text-decoration: none;
+}
+html {
+  overflow-x: hidden;
+}
+
 `;
 
 export default GlobalStyle;
