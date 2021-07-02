dcf9154d58bb6f05a2ff1da8ebdf098ba7d3cc04
fghjjkl32 <fghjjkl32@naver.com>
2021-07-02 10:50:47 +0900

    a

--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ export const GET_EDITINFO_API = `${BASE_URL}/users/nickname-check`;
 export const GET_SIGNIN_API = `${BASE_URL}/users/signin`;
 export const GET_SIGNUP_API = `${BASE_URL}/users/signup`;
 export const API = `${BASE_URL}`;
+export const MYPAGE_API = `${BASE_URL}`;
