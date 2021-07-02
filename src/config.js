216ea91c0c4a3b98f15b7ccdc9f4d8f4103bf43e
mac <mac@Bingbing.local>
2021-07-02 11:53:13 +0900

    Add: [21기 권오현] fix

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ const BASE_URL = 'http://10.58.7.41:8000';
 export const GET_EDITINFO_API = `${BASE_URL}/users/nickname-check`;
 export const GET_SIGNIN_API = `${BASE_URL}/users/signin`;
 export const GET_SIGNUP_API = `${BASE_URL}/users/signup`;
-export const API = `${BASE_URL}`;
-export const MYPAGE_API = `${BASE_URL}`;
+export const API = 'http://10.58.7.41:8000';
